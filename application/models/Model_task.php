191258e666df5c24909b6252c375a1bc883a0a96
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-23 10:14:30 +0700

    delete join ke table materi pada query get_tasks

--- application/models/Model_task.php
+++ application/models/Model_task.php
@@ -3,13 +3,12 @@
 class Model_task extends CI_Model {
 
 	public function get_tasks($username){
-		$this->db->select('t.*, tc.teacher_name, sj.subject_name, m.note as note_materi');
+		$this->db->select('t.*, tc.teacher_name, sj.subject_name');
 		// $this->db->select('s.*');
 		$this->db->from('student s');
 		$this->db->join('task t', 't.class_id = s.class_id', 'left');
 		$this->db->join('teacher tc', 'tc.teacher_id = t.teacher_id', 'left');
-		$this->db->join('materi m', 'm.materi_id = t.materi_id', 'left');
-		$this->db->join('subject sj', 'sj.subject_id = m.subject_id', 'left');
+		$this->db->join('subject sj', 'sj.subject_id = t.subject_id', 'left');
 		$this->db->where('s.nis', $username);
 
 		return $this->db->get()->result_array();
