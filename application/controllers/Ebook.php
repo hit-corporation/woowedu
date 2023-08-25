0cd741a6220325ca8008451bb8f3da43aba53274
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-25 17:32:39 +0700

    create function ebook search and UI ebook

--- application/controllers/Ebook.php
+++ application/controllers/Ebook.php
@@ -18,6 +18,25 @@ class Ebook extends CI_Controller {
 		$this->load->view('footer');
 	}
 
+	public function history(){
+		$username 	= $this->session->userdata('username');
+		$user_level = $this->db->where('username', $username)->get('users')->row_array()['user_level'];
+		$page 		= isset($_GET['page']) ? (int)$_GET['page'] : 1;
+		$limit 		= isset($_GET['limit']) ? (int)$_GET['limit'] : 3;
+		$title		= $_GET['title'];
+		$startDate	= $_GET['startDate'];
+		$endDate	= $_GET['endDate'];
 
+		$page = ($page - 1) * $limit;
+
+		$data['user_level'] 	= $user_level;
+		$data['news'] 			= $this->model_news->get_history($limit, $page, $title, $startDate, $endDate);
+		$data['total_records'] 	= $this->model_news->get_total_history($title, $startDate, $endDate);
+		$data['total_pages'] 	= ceil($data['total_records'] / $limit);
+
+		// create json header	
+		header('Content-Type: application/json');
+		echo json_encode($data);
+	}
 
 }
---
+++ application/views/ebook/index.php
@@ -0,0 +1,168 @@
+<section class="explore-section section-padding" id="section_2">
+	
+<div class="container">
+
+	<h4>Ebook</h4>
+
+	<!-- section search -->
+	<div class="row mt-4">
+
+		<div class="col-lg-6 col-md-6 col-sm-12">
+			<div class="mb-3 row">
+				<label for="judul" class="col-xl-2 col-lg-4 col-md-4 col-sm-2 col-form-label">Judul Buku</label>
+				<div class="col-xl-10 col-lg-8 col-md-8 col-sm-10">
+					<input type="text" class="form-control" id="judul" name="judul">
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-12">
+			<div class="mb-3 row">
+				<label for="category" class="col-lg-2 col-md-2 col-sm-2 col-form-label">Kategori</label>
+				<div class="col-lg-8 col-md-8 col-sm-8">
+					<input type="text" class="form-control" id="category" name="category">
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-2 d-flex justify-content-end">
+					<button class="btn btn-clear border shadow-sm" id="search">Cari</button>
+				</div>
+			</div>
+		</div>
+
+		
+	</div>
+
+	<div class="row mt-4">
+		<div class="container d-flex justify-content-end p-0">
+			<a href="<?=base_url()?>news/create" class="btn btn-success">
+				+ Buat Pengumuman
+			</a>
+		</div>
+	</div>
+
+	<!-- content -->
+	<div class="row mt-4" id="news-content">
+
+		
+		
+	</div>
+		
+	<nav aria-label="Page navigation example" class="d-flex justify-content-center">
+		<ul class="pagination">
+			
+		</ul>
+	</nav>
+	
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+	var currentPage = 1;
+	load_data(1,10);
+
+	// KETIKA BUTTON CARI DI KLIK
+	$('#search').on('click', function(e){
+		load_data();
+	});
+
+	// create function load data
+	function load_data(page = 1, limit = 10){
+		let title = $('#judul').val();
+		let category = $('#category').val();
+
+		$.ajax({
+			type: "GET",
+			url: BASE_URL+"ebook/history",
+			data: {
+				page: page,
+				limit: limit,
+				filter:{
+					title: title,
+					category: category
+				}
+				
+			},
+			success: function (response) {
+				$('#news-content').html('');
+				$.each(response.news, function (key, value){
+					let desc = value.isi;
+
+					if(desc.length > 100) desc = desc.substring(0, 300) + ' ...'
+
+					$('#news-content').append(`
+						<div class="container border rounded-4 bg-clear p-3 mb-3 news-item">
+							<div class="d-flex justify-content-between">
+								<a href="news/detail/${value.id}"><h6 class="mb-2">${value.judul}</h6></a>
+								<p style="font-size: 14px;">${value.tanggal}</p>
+							</div>
+							<p style="font-size: 14px;">${desc}</p>
+							<div class="container d-flex justify-content-end">${buttonGroup(response.user_level, value.id)}</div>
+						</div>
+					`);
+				});
+
+				$('.pagination').html('');
+				for(let i = 0; i < response.total_pages; i++){
+					if(currentPage == i+1){
+						$('.pagination').append(`
+							<li class="page-item active"><a class="page-link" href="#" onclick="page(${i+1}, this)">${i+1}</a></li>
+						`);
+					}else{
+						$('.pagination').append(`
+							<li class="page-item"><a class="page-link" href="#" onclick="page(${i+1}, this)">${i+1}</a></li>
+						`);
+					}
+
+				}
+			}
+		});
+	}
+
+	// JIKA PAGE NUMBER DI KLIK
+	function page(pageNumber, e){
+		currentPage = pageNumber;
+		load_data(pageNumber);
+	}
+
+	// BUTTON GROUP EDIT & DELETE
+	function buttonGroup(user_level, id){
+		let buttonGroup = `<a href="${BASE_URL+'news/create/'+id}" class="btn btn-clear border d-inline me-1 rounded-5"><i class="bi bi-pencil-square"></i></a>
+							<a class="btn btn-clear border d-inline rounded-5" onclick="deleteNews(${id})"><i class="bi bi-trash3-fill"></i></a>`;
+		if(user_level == 3 || user_level == 6){
+			return buttonGroup;
+		}
+
+		return '';
+	}
+
+	function deleteNews(id){
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!'
+		}).then((result) => {
+			if (result.isConfirmed) {
+
+				$.ajax({
+					type: "POST",
+					url: BASE_URL+"news/delete",
+					data: {
+						id: id
+					},
+					dataType: "JSON",
+					success: function (response) {
+						if(response.success == true){
+							Swal.fire('Deleted!', response.message, 'success');
+							window.location.href = BASE_URL+'news';
+						}
+					}
+				});
+
+				
+			}
+		})
+	}
+</script>
