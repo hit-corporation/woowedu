2257829a464a8dccbe67f48733496f2057480d97
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-28 13:22:47 +0700

    change limit query get_news

--- application/models/Model_news.php
+++ application/models/Model_news.php
@@ -3,7 +3,7 @@
 class Model_news extends CI_Model {
 
 	public function get_news(){
-		$this->db->limit('5');
+		$this->db->limit('3');
 		$this->db->order_by('tanggal', 'desc');
 		return $this->db->get('news')->result_array();
 	}
