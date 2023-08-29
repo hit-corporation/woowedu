71bd42625270c1b5b8f074961b7a6b8d771e8acd
naquib-hit <naquib@hitcorporation.com>
2023-08-29 15:32:23 +0700

    Update ebook.js

--- assets/js/ebook.js
+++ assets/js/ebook.js
@@ -14,7 +14,7 @@ const createBookGrid = e => {
     const imgWidth = 78 * 1.5;
     const imgHeight = 105 * 1.5;
     const column = document.createElement('div');
-    const figure = document.createElement('div');
+    const figure = document.createElement('a');
     const img = new Image(imgWidth, imgHeight);
     const figcaption = document.createElement('div');
     const title = document.createElement('p');
@@ -25,10 +25,20 @@ const createBookGrid = e => {
     figcaption.appendChild(title);
     // styles and attr
     column.classList.add('col-4', 'p-2');
-    figure.classList.add('card', 'flex-row', 'flex-nowrap', 'justify-content-around');
+    figure.classList.add('card', 'flex-row', 'flex-nowrap', 'justify-content-around', 'border-0', 'shadow-sm');
+    figure.style.cursor = 'pointer';
     figcaption.classList.add('card-body', 'flex-grow-1');
     img.classList.add('my-2', 'ms-2');
     title.classList.add('title', 'p-0');
+    // figure
+    figure.onmouseover = e => { 
+        figure.classList.remove('shadow-sm');
+        figure.classList.add('shadow'); 
+    };
+    figure.onmouseout = e => { 
+        figure.classList.remove('shadow'); 
+        figure.classList.add('shadow-sm'); 
+    };
     // image
     const imgFileName = ('assets/images/ebooks/cover/' + e.cover_img).split('.');
     img.src = imgFileName[0] + '_thumb.jpg';
@@ -73,7 +83,7 @@ const getBooks = async (page, count) => {
     const ebooks = await getBooks(1, 9);
 
     grid.innerHTML = null;
-    Array.from([...await ebooks.data], props => {
+    Array.from(ebooks.data, props => {
         grid.appendChild(createBookGrid(props));
     });
 
