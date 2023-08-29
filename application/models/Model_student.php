795882297131bad9b4d84622254df68ee47eab7b
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-29 16:25:25 +0700

    create detail tugas student

--- application/models/Model_student.php
+++ application/models/Model_student.php
@@ -28,4 +28,46 @@ class Model_student extends CI_Model {
 		return $query->num_rows();
 	}
 
+	public function get_total_exam($class_id, $start_dt, $end_dt){
+		$this->db->where('class_id', $class_id);
+		$this->db->where('DATE(start_date) >=', $start_dt);
+		$this->db->where('DATE(end_date) <=', $end_dt);
+		return $this->db->get('exam')->num_rows();
+	}
+
+	public function average_exam_score($student_id, $start_dt, $end_dt){
+		$this->db->select('AVG(exam_total_nilai) as exam_total_nilai');
+		$this->db->where('student_id', $student_id);
+		$this->db->where('DATE(exam_submit) >=', $start_dt);
+		$this->db->where('DATE(exam_submit) <=', $end_dt);
+		return $this->db->get('exam_student')->row_array();
+	}
+
+	public function get_task($limit = null, $page = null, $student_id){
+		$class_id = $this->get_class($student_id);
+
+		$this->db->select('t.*, m.title');
+		$this->db->from('task t');
+		$this->db->join('materi m', 'm.materi_id = t.materi_id');
+		$this->db->where('class_id', $class_id);
+		$this->db->order_by('available_date', 'desc');
+		$this->db->limit($limit, $page);
+		return $this->db->get()->result_array();
+	}
+
+	public function get_total_task($student_id){
+		$class_id = $this->get_class($student_id);
+
+		$this->db->select('t.*, m.title');
+		$this->db->from('task t');
+		$this->db->join('materi m', 'm.materi_id = t.materi_id');
+		$this->db->where('class_id', $class_id);
+		return $this->db->get()->num_rows();
+	}
+
+	public function get_class($student_id){
+		$this->db->where('student_id', $student_id);
+		return $this->db->get('student s')->row_array()['class_id'];
+	}
+
 }
--- application/views/student/detail.php
+++ application/views/student/detail.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-3">
-				<div class="container border rounded p-3">
+				<div class="card border rounded p-3 h-100">
 					<div class="image-content">
 						<span>
 							<img src="<?=base_url('assets/images/users/').$detail['photo']?>" alt="" width="50">
@@ -26,7 +26,7 @@
 				</div>
 			</div>
 			<div class="col-xl-5 col-lg-9 col-md-6 col-sm-12 col-xs-12 mb-3 h-100">
-				<div class="container border rounded p-3 data-by-date">
+				<div class="card border rounded p-3 data-by-date h-100">
 					<input class="border-width-1 rounded-lg ml-3"  style="height: 40px; text-align:center; border-color: rgba(0, 0, 255, 0.3);" type="text" name="daterange" 
 						value="<?php 
 						if(isset($start)){ 
@@ -39,11 +39,60 @@
 						}else{ 
 							echo date('m/d/Y', time()); 
 						}?>" />
+
+					<div class="row data-content mt-4">
+						<div class="col-3 total-exam text-center">
+							<h4>0</h4>
+							<span>Total Tugas</span>
+						</div>
+						<div class="col-3 ave-exam-score text-center">
+							<h4>0</h4>
+							<span>Skor Rata-rata</span>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-3">
+			<!-- <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-3">
 				<div class="container border rounded">
 					tes
+				</div>
+			</div> -->
+		</div>
+	</div>
+
+	<div class="container p-4">
+		<div class="row border rounded">
+			<h6 class="text-center mt-4">Laporan Kinerja Siswa</h6>
+			<nav>
+				<div class="nav nav-tabs" id="nav-tab" role="tablist">
+					<button class="nav-link active" id="nav-ebook-tab" data-bs-toggle="tab" data-bs-target="#nav-ebook" type="button" role="tab" aria-controls="nav-ebook" aria-selected="true">Ebook</button>
+					<button class="nav-link" id="nav-tugas-tab" data-bs-toggle="tab" data-bs-target="#nav-tugas" type="button" role="tab" aria-controls="nav-tugas" aria-selected="false">Tugas</button>
+					<button class="nav-link" id="nav-ujian-tab" data-bs-toggle="tab" data-bs-target="#nav-ujian" type="button" role="tab" aria-controls="nav-ujian" aria-selected="false">Ujian</button>
+				</div>
+			</nav>
+			<div class="tab-content mb-4" id="nav-tabContent">
+				<div class="tab-pane fade show active" id="nav-ebook" role="tabpanel" aria-labelledby="nav-ebook-tab" tabindex="0">
+					
+				</div>
+				<div class="tab-pane fade" id="nav-tugas" role="tabpanel" aria-labelledby="nav-tugas-tab" tabindex="0">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Nama Tugas</th>
+								<th>Ditugaskan</th>
+								<th>Batas waktu</th>
+								<th>Tanggal penyerahan</th>
+								<th>File</th>
+								<th>Notes</th>
+							</tr>
+						</thead>
+						<tbody id="tugas-body-content">
+
+						</tbody>
+					</table>
+				</div>	
+				<div class="tab-pane fade" id="nav-ujian" role="tabpanel" aria-labelledby="nav-ujian-tab" tabindex="0">
+					Ujian Content
 				</div>
 			</div>
 		</div>
@@ -56,6 +105,13 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
 <script>
+	var currentPage = 1;
+	var student_id 	= <?=$detail['student_id']?>;
+	let startDate 	= moment().startOf('month').startOf('day').format('YYYY-MM-DD');
+	let endDate		= moment().startOf('day').format('YYYY-MM-DD');
+
+	getSummary(student_id, startDate, endDate);
+
 	$(document).ready(function () {
 		$('input[name="daterange"]').daterangepicker({
 			opens: 'right',
@@ -70,7 +126,75 @@
 				}
 			}, function(start, end, label) {
 				console.log(start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+
+				getSummary(student_id, start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'));
 			}
 		);
 	});
+
+	// FUNGSI UNTUK UBAH DATA CONTENT SUMMARY
+	function getSummary(student_id, start, end){
+		$.ajax({
+			type: "POST",
+			url: BASE_URL+"student/get_summary",
+			data: {
+				student_id, student_id,
+				start: start,
+				end: end
+			},
+			dataType: "JSON",
+			success: function (response) {
+				$('.total-exam')[0].children[0].innerHTML = response.total_exam;
+				$('.ave-exam-score')[0].children[0].innerHTML = response.average_exam_score;
+			}
+		});
+	}
+
+	// FUNGSI UNTUK ISI LIST DATA TUGAS
+	function getTask(page = 1, limit = 10, student_id){
+		$.ajax({
+			type: "GET",
+			url: BASE_URL+"student/get_task",
+			data: {
+				page: page,
+				limit: limit,
+				student_id: student_id
+			},
+			success: function (response) {
+				$('#tugas-body-content').html('');
+				$.each(response.data, function (key, value){
+					$('#tugas-body-content').append(`
+						<tr>
+							<td>${value.title}</td>
+							<td>${value.available_date}</td>
+							<td>${value.due_date}</td>
+							<td>${value.task_submit}</td>
+							<td><a href="${BASE_URL+`assets/files/student_task/`+value.task_file_answer}">${value.task_file_answer}</a></td>
+							<td>${value.note}</td>
+						</tr>
+					`);
+				});
+
+				$('.pagination').html('');
+				for(let i = 0; i < response.total_pages; i++){
+					if(currentPage == i+1){
+						$('.pagination').append(`
+							<li class="page-item active"><a class="page-link" href="#" onclick="page(${i+1}, this)">${i+1}</a></li>
+						`);
+					}else{
+						$('.pagination').append(`
+							<li class="page-item"><a class="page-link" href="#" onclick="page(${i+1}, this)">${i+1}</a></li>
+						`);
+					}
+
+				}
+			}
+		});
+	}
+
+	// JIKA nav-tugas-tab DI KLIK
+	$('#nav-tugas-tab').on('click', function(){
+		getTask(1, 10, student_id);
+	});
 </script>
+a
