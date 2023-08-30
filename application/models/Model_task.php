9e68f53e08488366d2cc6303369792e749c71eb9
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-30 14:44:45 +0700

    create fitur submit tugas

--- application/models/Model_task.php
+++ application/models/Model_task.php
@@ -4,7 +4,6 @@ class Model_task extends CI_Model {
 
 	public function get_tasks($username){
 		$this->db->select('t.*, tc.teacher_name, sj.subject_name');
-		// $this->db->select('s.*');
 		$this->db->from('student s');
 		$this->db->join('task t', 't.class_id = s.class_id', 'left');
 		$this->db->join('teacher tc', 'tc.teacher_id = t.teacher_id', 'left');
@@ -12,6 +11,16 @@ class Model_task extends CI_Model {
 		$this->db->where('s.nis', $username);
 
 		return $this->db->get()->result_array();
+	}
+
+	public function get_tasks_detail($id){
+		$this->db->select('t.*, tc.teacher_name, sj.subject_name');
+		$this->db->from('task t');
+		$this->db->join('teacher tc', 'tc.teacher_id = t.teacher_id', 'left');
+		$this->db->join('subject sj', 'sj.subject_id = t.subject_id', 'left');
+		$this->db->where('t.task_id', $id);
+
+		return $this->db->get()->row_array();
 	}
 
 }
