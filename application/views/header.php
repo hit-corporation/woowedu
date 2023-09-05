e5dc6369c38140978fab7b2dd80be9e9307058e9
fauziahmad37 <fauzi.enginer@gmail.com>
2023-09-05 15:04:42 +0700

    change link logo small icon

--- application/views/header.php
+++ application/views/header.php
@@ -85,7 +85,7 @@
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
                     <a class="navbar-brand pt-0" href="#">
-                        <img src="<?= base_url('assets/images/logo-small-in.png') ?>" alt="" width="120">
+                        <img src="<?= base_url('assets/images/logo-small-in2.png') ?>" alt="" width="120">
                     </a>
 
                     <div class="d-lg-none ms-auto me-4">
