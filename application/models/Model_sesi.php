35fe865a73db4c4d8e77c9359586d234fc2e7335
hasmi <hasmi@hitcorporation.com>
2023-09-11 16:07:27 +0700

    update sesi

--- application/models/Model_sesi.php
+++ application/models/Model_sesi.php
@@ -8,8 +8,8 @@ class Model_sesi extends CI_Model {
   
 		$this->db->select('sesi_id,	sesi_title,	sesi_date,sesi_jam_start,	sesi_jam_end,	teacher_id');
 		$this->db->from('sesi');	 
-		$this->db->where('sesi_date >=',$param['sdate']); 
-		$this->db->where('sesi_date <=',$param['edate']); 
+		$this->db->where('sesi_date >=',date("Y-m-d",strtotime($param['sdate']))); 
+		$this->db->where('sesi_date <=',date("Y-m-d",strtotime($param['edate']))); 
 		$this->db->where('teacher_id',$param['teacher_id']); 
 
 		$query = $this->db->get();
