ef97b7f7e689507f9a0175d6a8d1155c89f82210
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-03-18 08:43:01 +0700

    fixing error di bagian looping teacher subject

--- admin/application/views/teacher/relasi.php
+++ admin/application/views/teacher/relasi.php
@@ -11,8 +11,10 @@
 								$teacher_id =  $_GET['id'];
 								$teacher_class = $this->db->where('teacher_id',$teacher_id)->get('class_teacher');
 								
+								$_arr_teacher_class = [];
 								foreach ($teacher_class->result() as $obj) $_arr_teacher_class[] = $obj->class_id;
 								
+								$_arr_teacher_subject = [];
 								$teacher_subject = $this->db->where('teacher_id',$teacher_id)->get('subject_teacher');
 								foreach ($teacher_subject->result() as $obj) $_arr_teacher_subject[] = $obj->subject_id;
 								
@@ -38,12 +40,12 @@
 								$subject = $this->db->order_by('subject_id')->get('subject');
 								$i=0; 
 								foreach ($subject->result() as $obj) { 
-									if(in_array($obj->class_id,$_arr_teacher_subject)){
+									if(in_array($obj->subject_id,$_arr_teacher_subject)){
 									?>
-									<label><input class="check" type="checkbox teacher_subject" name="teacher_subject[]"  checked     class="filled-in" value="<?=$obj->subject_id?>" /></label>	
+									<label><input type="checkbox" name="teacher_subject[]"  checked class="filled-in teacher_subject"  value="<?=$obj->subject_id?>" /></label>	
 									&nbsp;&nbsp;&nbsp;<?=$obj->subject_name?><br />
 									<?php }else{ ?>
-									<label><input class="check teacher_subject" type="checkbox" name="teacher_subject[]"  class="filled-in" value="<?=$obj->subject_id?>" /></label>	
+									<label><input type="checkbox" name="teacher_subject[]"  class="filled-in teacher_subject" value="<?=$obj->subject_id?>" /></label>	
 									&nbsp;&nbsp;&nbsp;<?=$obj->subject_name?><br />
 									<?php
 									$i++;
