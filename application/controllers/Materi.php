a681528ab1564605d3df41dda05d636ef4600d60
Ahmad Fauzi <fauzi.enginer@gmail.com>
2024-03-19 09:50:26 +0700

    create function materi_saya dan store_materi_saya pada controller Materi

--- application/controllers/Materi.php
+++ application/controllers/Materi.php
@@ -52,6 +52,16 @@ class Materi extends CI_Controller {
 		$filter = $this->input->get('columns');
 		$data   = $this->model_mapel->get_all($limit, $offset, $filter);
 
+
+		foreach ($data as $key => $val) {
+			$dir = str_replace('\application\controllers','',__DIR__).'/assets/files/materi/'; // get direktory file
+			if(file_exists($dir.$val['materi_file'])){
+				$data[$key]['file_size'] = filesize($dir.$val['materi_file']);
+			}else{
+				$data[$key]['file_size'] = 0;
+			}
+		}
+
 		$json = [
 			'draw'				=> $draw,
 			'data' 		   		=> $data,
@@ -91,5 +101,118 @@ class Materi extends CI_Controller {
 
 		header('Content-Type: application/json');
 		echo json_encode($materies, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_QUOT);
+	}
+
+	public function materi_saya(){
+		$header['add_css'] = [
+			'https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css',
+			'assets/libs/sweetalert2/sweetalert2.min.css',
+			'assets/node_modules/pagination-system/dist/pagination-system.min.css',
+		];
+
+		$data['page_js'][] = ['path' => 'https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js'];
+		$data['page_js'][] = ['path' => 'https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js'];
+		$data['page_js'][] = ['path' => 'assets/libs/sweetalert2/sweetalert2.min.js'];
+		$data['page_js'][] = ['path' => 'https://kit.fontawesome.com/b377b34fd7.js'];
+
+		$teacher_id = $_SESSION['teacher_id'];
+		// $data['materies'] = $this->db->where('teacher_id', $teacher_id)->get('materi')->result_array();
+
+		// foreach ($data['materies'] as $key => $val) {
+		// 	$dir = str_replace('\application\controllers','',__DIR__).'/assets/files/materi/'; // get direktory file
+		// 	if(file_exists($dir.$val['materi_file'])){
+		// 		$data['materies'][$key]['file_size'] = filesize($dir.$val['materi_file']);
+		// 	}else{
+		// 		$data['materies'][$key]['file_size'] = 0;
+		// 	}
+		// }
+
+		$data['mapels'] = $this->db->where('teacher_id', $teacher_id)->join('subject s', 's.subject_id=st.subject_id')->get('subject_teacher st')->result_array();
+
+		$this->load->view('header', $header);
+		$this->load->view('mapel/materi_saya', $data);
+		$this->load->view('footer');
+	}
+
+	public function list_materi_saya(): void {
+		$draw	= $this->input->get('draw') ?? '';
+		$limit  = $this->input->get('length');
+		$offset = $this->input->get('start');
+		$count  = $this->db->count_all_results('materi');
+		$filter = $this->input->get('columns');
+
+		$filter[0]['search']['value'] = isset($filter[0]['search']['value']) ? $_SESSION['teacher_id'] : null;
+		
+		$data   = $this->model_mapel->get_all_materi_saya($limit, $offset, $filter);
+
+
+		foreach ($data as $key => $val) {
+			$dir = str_replace('\application\controllers','',__DIR__).'/assets/files/materi/'; // get direktory file
+			if(file_exists($dir.$val['materi_file'])){
+				$data[$key]['file_size'] = filesize($dir.$val['materi_file']);
+			}else{
+				$data[$key]['file_size'] = 0;
+			}
+		}
+
+		$json = [
+			'draw'				=> $draw,
+			'data' 		   		=> $data,
+			'recordsTotal' 		=> count($data),
+			'recordsFiltered'	=> $this->model_mapel->num_all_materi_saya($filter)
+		];
+
+		header('Content-Type: application/json');
+		echo json_encode($json, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_QUOT);
+	}
+
+	public function store_materi_saya(){
+		$post 		= $this->input->post();
+		$title		= trim($post['input_materi']);
+		$materi_id	= ($post['materi_id'] != '') ? $post['materi_id'] : null;
+		$file_name 	= $_FILES['input_file']['name'];
+
+		$subject = $this->db->where('subject_id', $post['subject_id'])->get('subject')->row_array();
+
+		// upload file
+		$dir = 'assets/files/materi/';
+		if(!is_dir($dir))  @mkdir($dir, 0777);
+		$filename = str_replace(' ', '_', strtolower($subject['subject_name'])).'-'.str_replace(' ', '_', strtolower($post['input_materi']));
+		$ext = pathinfo(basename($file_name), PATHINFO_EXTENSION);
+		$move = move_uploaded_file($_FILES['input_file']['tmp_name'], FCPATH.$dir.$filename.'.'.$ext);
+
+		if(!$move){
+			$message = ['success' => false, 'message' => 'File gagal di upload !!!'];
+		}else{ 
+
+			// cek materi sudah pernah ada atau belum		
+			$cek = $this->db->where('materi_id', $materi_id)->get('materi')->num_rows();
+			
+			if($cek > 0){
+				// edit
+				$data = [
+					'title' 	=> $title,
+					'edit_at' 	=> date('Y-m-d H:i:s'),
+				];
+				$this->db->update('materi', $data, ['subject_id' => $post['subject_id']]);
+				$message = ['success' => true, 'message' => 'Data berhasil di update !!!'];
+			}else{
+				// save
+				$data = [
+					'subject_id' 		=> $post['subject_id'],
+					'teacher_id'		=> $_SESSION['teacher_id'],
+					'title' 			=> $title,
+					'available_date' 	=> date('Y-m-d H:i:s'),
+					'edit_at'			=> date('Y-m-d H:i:s'),
+					'materi_file'       => $filename.'.'.$ext,
+				];
+				$this->db->insert('materi', $data);
+				$message = ['success' => true, 'message' => 'Data berhasil di simpan !!!'];
+			}
+		}
+		
+		$this->session->set_flashdata('success', $message);
+		redirect('materi/materi_saya');
+
 	}
 }
